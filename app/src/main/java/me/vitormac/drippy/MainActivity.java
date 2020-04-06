fdc3fbbdb2f87ec862fd80e53a2ec15167bf6cc4
VitorMac10 <vitormac3@hotmail.com>
2020-04-06 11:28:22 -0300

    Added Webview go back action

--- app/src/main/java/me/vitormac/drippy/MainActivity.java
+++ app/src/main/java/me/vitormac/drippy/MainActivity.java
@@ -1,22 +1,30 @@
 package me.vitormac.drippy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.Window;
 import android.webkit.WebView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView webView = new WebView(this);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.addJavascriptInterface(new JavascriptBridge(this), "native");
-        webView.getSettings().setDomStorageEnabled(true);
-        webView.loadUrl("https://drippy-music.github.io/");
-        setContentView(webView);
+        this.webView = new WebView(this);
+        this.webView.getSettings().setJavaScriptEnabled(true);
+        this.webView.addJavascriptInterface(new JavascriptBridge(this), "native");
+        this.webView.getSettings().setDomStorageEnabled(true);
+        this.webView.loadUrl("https://drippy-music.github.io/");
+        this.setContentView(this.webView);
     }
+
+    @Override
+    public void onBackPressed() {
+        if (this.webView.canGoBack()) this.webView.goBack();
+        else super.onBackPressed();
+    }
+
 }
