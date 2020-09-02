2b16ae9da4a3639a1646132b5371e55e37ec4910
VitorMac10 <vitormac3@hotmail.com>
2020-09-01 21:36:35 -0300

    MainActivity update check & local files handling with WebView

--- app/src/main/java/me/vitormac/drippy/MainActivity.java
+++ app/src/main/java/me/vitormac/drippy/MainActivity.java
@@ -1,14 +1,25 @@
 package me.vitormac.drippy;
 
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.webkit.WebViewAssetLoader;
 
+import java.io.File;
+
+import me.vitormac.drippy.bootstrap.AutoUpdater;
 import me.vitormac.drippy.model.MediaManager;
+import me.vitormac.drippy.webview.RouteHandler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String REPO_URL = "https://api.github.com/repos/DrippyMusic/drippy.live/releases/latest";
 
     private WebView webView;
 
@@ -17,12 +28,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         MediaManager.create(this);
 
-        this.webView = new WebView(this);
-        this.webView.setWebViewClient(new WebViewClient());
-        this.webView.getSettings().setJavaScriptEnabled(true);
-        this.webView.getSettings().setDomStorageEnabled(true);
-        this.webView.loadUrl("https://drippy.live");
-        this.setContentView(this.webView);
+        File dist = new File(this.getFilesDir().getAbsolutePath() + "/dist");
+        WebViewAssetLoader loader = new WebViewAssetLoader.Builder().setDomain("drippy.live")
+                .addPathHandler("/", new RouteHandler(dist)).build();
+
+        new AutoUpdater(this, dist, () -> {
+            this.webView = new WebView(this);
+            this.webView.setWebViewClient(new WebViewClient() {
+                @Override
+                public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+                    return loader.shouldInterceptRequest(request.getUrl());
+                }
+
+                @Override
+                public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                    // TODO trust Drippy's API certs and stuff, below code unsafe
+                    handler.proceed();
+                }
+            });
+
+            this.webView.getSettings().setJavaScriptEnabled(true);
+            this.webView.getSettings().setDomStorageEnabled(true);
+            this.webView.loadUrl("https://drippy.live");
+            this.setContentView(this.webView);
+        }).execute(REPO_URL);
     }
 
     @Override
