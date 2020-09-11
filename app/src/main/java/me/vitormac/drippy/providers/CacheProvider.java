3aaceb3657ca772b6124a7375d3cad524eab934e
VitorMac10 <vitormac3@hotmail.com>
2020-09-10 21:13:12 -0300

    Partial content response for cache streaming

--- app/src/main/java/me/vitormac/drippy/providers/CacheProvider.java
+++ app/src/main/java/me/vitormac/drippy/providers/CacheProvider.java
@@ -4,6 +4,7 @@ import android.webkit.WebResourceResponse;
 
 import com.google.gson.JsonObject;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -23,12 +24,16 @@ public class CacheProvider extends ProviderBase<DataModel> {
 
     @Override
     public WebResourceResponse stream(String range) throws IOException {
+        long length = this.file.length();
         Map<String, String> headers = new HashMap<String, String>() {{
             put("Access-Control-Allow-Origin", "*");
+            put("Accept-Ranges", "bytes");
+            put("Content-Range", String.format("bytes 0-%s/%s", length - 1, length));
         }};
 
         return new WebResourceResponse("application/octet-stream", null,
-                200, "OK", headers, new FileInputStream(this.file));
+                206, "Partial Content", headers,
+                new BufferedInputStream(new FileInputStream(this.file)));
     }
 
     @Override
