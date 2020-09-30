24b2059edff9329d686b608c8d0c7dea89851be1
VitorMac10 <vitormac3@hotmail.com>
2020-09-29 22:07:36 -0300

    Internal server basic implementation

--- app/src/main/java/me/vitormac/drippy/webview/InternalServer.java
+++ app/src/main/java/me/vitormac/drippy/webview/InternalServer.java
@@ -1,9 +1,19 @@
 package me.vitormac.drippy.webview;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import fi.iki.elonen.NanoHTTPD;
+import me.vitormac.drippy.codec.FFmpeg;
+import me.vitormac.drippy.providers.ProviderBase;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.ResponseBody;
 
 public final class InternalServer extends NanoHTTPD {
 
@@ -26,6 +36,24 @@ public final class InternalServer extends NanoHTTPD {
     }
 
     private Response request(IHTTPSession session) throws IOException {
+        Map<String, List<String>> parameters = session.getParameters();
+        if (parameters.containsKey("id")) {
+            String id = parameters.get("id").get(0);
+
+            Request.Builder request = new Request.Builder().url(API_URL + "/data/" + id);
+            try (okhttp3.Response response = this.client.newCall(request.build()).execute()) {
+                ResponseBody body = Objects.requireNonNull(response.body());
+                JsonObject data = JsonParser.parseString(body.string())
+                        .getAsJsonObject();
+
+                ProviderBase<?> provider = DrippyUtils.getProvider(data, id);
+                FFmpeg ffmpeg = new FFmpeg(provider.stream());
+
+                return NanoHTTPD.newChunkedResponse(Response.Status.OK,
+                        "audio/opus", ffmpeg.stdout());
+            }
+        }
+
         return super.serve(session);
     }
 
